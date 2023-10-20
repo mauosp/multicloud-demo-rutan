20494ad720ab680e18dcc13668463e9055533f0f
mauosp <55215654+mauosp@users.noreply.github.com>
2023-10-20 02:05:32 +0000

    Update CI

--- Lambda/HelloRutaNLambda/index.js
+++ Lambda/HelloRutaNLambda/index.js
@@ -4,6 +4,6 @@ exports.handler = async (event, context) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
     console.log('Community Info: ', event.message);
     console.log('Console for test #2');
-    return "Hello People From The UdeA";
+    return "Hello People From Ruta N";
     // throw new Error('Something went wrong');
 };
